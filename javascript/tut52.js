6d89ae4efda4ac6aaf10f1c187268887d1c47d62
ujjala kumari <113276682+ujjalakumari@users.noreply.github.com>
2022-12-01 21:28:12 +0530

    Add files via upload

---
+++ javascript/tut52.js
@@ -0,0 +1,23 @@
+console.log("this is tutorial53");
+function greet(name, greetText){
+    console.log(greetText+""+name);
+    console.log(name + " is a good boy");
+}
+let name = "Harry";
+let name1 = "Shubham";
+let name2 = "Rohan";
+let name3 = "Sagar";
+ let greetText="Good Morning";
+greet(name ,greetText);
+greet(name1,greetText);
+greet(name2,greetText);
+greet(name3,greetText);
+
+// let name = "Harry";
+// let name1 = "Shubham";
+// let name2 = "Rohan";
+// let name3 = "Sagar";
+// console.log(name + “is a good boy”);
+// console.log(name1 + “is a good boy”);
+// console.log(name2 + “is a good boy”);
+// console.log(name3 + “is a good boy”);
